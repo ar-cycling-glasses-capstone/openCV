fc361c98406e7fb673eed43bda6f85eb8ff05c61
sabrinapunjani <sabrinaxjp@gmail.com>
2023-02-26 18:05:43 -0500

    added models

--- cardetection.py
+++ cardetection.py
@@ -1,27 +1,39 @@
 import cv2
 
 
-cars_cascade = cv2.CascadeClassifier('cars.xml')
+carsFront_cascade = cv2.CascadeClassifier('cars2.xml')
+carsBack_cascade = cv2.CascadeClassifier('cars.xml')
 body_cascade = cv2.CascadeClassifier('fullbody.xml')
+people_cascade = cv2.CascadeClassifier('pedestrian.xml')
 distanceCutoff = 100
 
 def detect_cars_and_pedestrain(frame):
-    cars = cars_cascade.detectMultiScale(frame, 1.15, 4)
+    cars1 = carsFront_cascade.detectMultiScale(frame, 1.15, 4)
+    cars2 = carsBack_cascade.detectMultiScale(frame, 1.15, 4)
+    people =  people_cascade.detectMultiScale(frame, 1.15, 4)
     pedistrain = body_cascade.detectMultiScale(frame, 1.15, 4)
-    for (x, y, w, h) in cars:
+    for (x, y, w, h) in cars1: 
         
+        #print(x, y, w, h)
         
-        
-        #cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 0), thickness=2)
-        if x<distanceCutoff:
-            print("too close")
+        if y<distanceCutoff:
+            
             cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(275,7,75), thickness=5)
         else:
             cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(275,7,75), thickness=2)
+    for (x, y, w, h) in cars2: 
+        
+        #print(x, y, w, h)
+        
+        if y<distanceCutoff:
             
-    '''for(x, y, w, h) in pedistrain:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) '''
-
+            cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(217, 255, 0), thickness=5)
+        else:
+            cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(217, 255, 0), thickness=2)       
+    for(x, y, w, h) in pedistrain:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
+    for(x, y, w, h) in people:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
     return frame
 
 def Simulator():
@@ -30,10 +42,10 @@ def Simulator():
     
     while CarVideo.isOpened():
         ret, frame = CarVideo.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#conv to grayscale for faster processing 
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#conv to grayscale for faster processing 
         controlkey = cv2.waitKey(1)
         if ret:        
-            cars_frame = detect_cars_and_pedestrain(gray)
+            cars_frame = detect_cars_and_pedestrain(frame)
             cv2.imshow('frame', cars_frame)
         else:
             break
