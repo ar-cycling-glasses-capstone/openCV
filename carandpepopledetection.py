050fbeb60fcddbdb63496177c9ad0b025150bcb0
sabrinapunjani <sabrinaxjp@gmail.com>
2023-04-09 15:56:17 -0400

    task

--- carandpepopledetection.py
+++ carandpepopledetection.py
@@ -2,8 +2,11 @@ import cv2
 #from imutils import paths
 import numpy as np
 #import imutils
+import schedule 
+import time 
 
 
+pollingrate = 2
 carsFront_cascade = cv2.CascadeClassifier('models/cars2.xml')
 carsBack_cascade = cv2.CascadeClassifier('models/cars.xml')
 body_cascade = cv2.CascadeClassifier('models/fullbody.xml')
@@ -11,34 +14,47 @@ people_cascade = cv2.CascadeClassifier('models/pedestrian.xml')
 distanceCutoff = 100
 
 def detect_cars_and_pedestrain(frame):
+    print("job run")
     cars1 = carsFront_cascade.detectMultiScale(frame, 1.15, 40)
     cars2 = carsBack_cascade.detectMultiScale(frame, 1.15, 40)
     people =  people_cascade.detectMultiScale(frame, 1.15, 40)
     pedistrain = body_cascade.detectMultiScale(frame, 1.15, 40)
+    
+    carDetected= False
+    peopleDetected = False
+    peoples = 0
+    cars = 0
+    result = []
     for (x, y, w, h) in cars1: 
         
         #print(x, y, w, h)
         
         if y<distanceCutoff:
-            
-            cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(275,7,75), thickness=5)
-        else:
-            cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(275,7,75), thickness=2)
+            carDetected = True
+            cars+=1
+        #     cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(275,7,75), thickness=5)
+        # else:
+        #     cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(275,7,75), thickness=2)
     for (x, y, w, h) in cars2: 
         
-        print(x, y, w, h)
+        
         
         if y<distanceCutoff:
-            
-            cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(217, 255, 0), thickness=5)
-        else:
-            cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(217, 255, 0), thickness=2)       
+            carDetected=True
+            cars+=1
+            #cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(217, 255, 0), thickness=5)
+            #else:
+            #cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(217, 255, 0), thickness=2)       
     for(x, y, w, h) in pedistrain:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
+        peopleDetected = True
+        peoples+=1
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
     for(x, y, w, h) in people:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
-    return frame
-
+        peopleDetected = True
+        peoples+=1
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
+    return peopleDetected, peoples, carDetected, cars
+   
 
 def distance_to_camera(knownWidth, focalLength, perWidth):
 	    # compute and return the distance from the maker to the camera
@@ -52,16 +68,22 @@ def Simulator():
     
     while CarVideo.isOpened():
         ret,frame = CarVideo.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#conv to grayscale
         controlkey = cv2.waitKey(1)
-        if ret:        
-            cars_frame = detect_cars_and_pedestrain(gray)
-            cv2.imshow('frame',cars_frame)
-            cv2.waitKey(100)
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)#conv to grayscale        
+            cars_frame = detect_cars_and_pedestrain(frame)
+            schedule.every(pollingrate).seconds.do(detect_cars_and_pedestrain, gray)
+            #cv2.imshow('frame',cars_frame)
+            
+            
+            
+            time.sleep(1)
         else:
             break
         if controlkey == ord('q'):
             break
+        people_detected, num_people, car_detected, num_cars= schedule.run_pending()
+        
 
     CarVideo.release()
     cv2.destroyAllWindows()
